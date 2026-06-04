23af435d086177a83e1c6ae120cb2fe17543bde9
David Gelerinter <dovie@uchicago.edu>
2026-06-04 07:38:46 -0500

    Another cleanup fix. -DJG

--- src/constants/resources.ts
+++ src/constants/resources.ts
@@ -2,6 +2,7 @@ export const resourceIdField: Record<string, string> = {
   genome: "genome_id",
   genome_sequence: "sequence_id",
   genome_feature: "patric_id",
+  protein_feature: "id",
   genome_amr: "id",
   strain: "strain",
   epitope: "epitope_id",
